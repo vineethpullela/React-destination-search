f438220ce1539a398e300511b465c2e587f79d6f
vineeth <15072cm031@gmail.com>
2022-12-06 05:58:34 +0000

    all test cases are passed

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,48 +1,49 @@
 import './index.css'
 import {Component} from 'react'
-
 import DestinationItem from '../DestinationItem'
 
 class DestinationSearch extends Component {
   state = {searchInput: ''}
 
-  onChangeSearchInput = event => {
+  onChangeInput = event => {
     this.setState({searchInput: event.target.value})
   }
 
   render() {
     const {searchInput} = this.state
-    const {destinationList} = this.props
+    const {destinationsList} = this.props
 
-    const searchResults = destinationList.filter(eachDestination =>
+    const searchResults = destinationsList.filter(eachDestination =>
       eachDestination.name.toLowerCase().includes(searchInput.toLowerCase()),
     )
 
+    console.log(searchResults)
+    console.log(searchInput)
     return (
       <div className="app-container">
         <div className="destination-search-container">
           <h1 className="main-heading">Destination Search</h1>
           <div className="search-input-container">
             <input
-              type="search-input"
-              value={searchInput}
+              type="search"
               className="search-input"
-              onChange={this.onChangeSearchInput}
+              value={searchInput}
+              onChange={this.onChangeInput}
             />
             <img
               src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
-              alt="search-Icon"
+              alt="search icon"
               className="search-icon"
             />
-            <ul className="cards-container">
-              {searchResults.map(eachDestination => (
-                <DestinationItem
-                  key={eachDestination.id}
-                  destinationDetails={eachDestination}
-                />
-              ))}
-            </ul>
           </div>
+          <ul className="destination-items-container">
+            {searchResults.map(eachDestination => (
+              <DestinationItem
+                destinationDetails={eachDestination}
+                key={eachDestination.id}
+              />
+            ))}
+          </ul>
         </div>
       </div>
     )
